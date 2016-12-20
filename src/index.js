dcfeb968cfb588bb88eb61429641e08a90f1ae91
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-20 14:16:34 -0800

    makin progress

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a component which produces some html
+
+const App = function () {
+    return <div>Hey There</div>
+}
+
+
+//take generated HTML and put into DOM
+
+ReactDOM.render(App);
