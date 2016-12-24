f1adb5bfc3f5f405629d326487765dd15ce6e434
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-23 21:36:43 -0800

    more layout

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+    // that video reference is functionally the same as: const video = props.video;
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
 
-    return <li>Video</li>
-
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
