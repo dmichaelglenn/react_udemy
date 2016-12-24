3334d79fd6d54e40086f6a6b592ecd2ebed7789e
Michael Glenn <dmichaelglenn@gmail.com>
2016-12-24 14:53:14 -0800

    its stating to work!

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-const VideoDetail = (props) => {
+const VideoDetail = ({video}) => {
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+
+
+    const videoId = video.id.videoId;
+    //in es6, the backticks let us interpolate a javascript object like below as opposed to just concatenating a string
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responive-item"></iframe>
+                <iframe className="embed-responive-item" src={url}></iframe>
             </div>
             <div className="details">
-                <div></div>
-                <div></div>
+    <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
             </div>
         </div>
     )
